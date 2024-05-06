a3e89e4ae70935e335f8d5aa5e6f7e7bcb03b1b6
Devarat Patel <devaratpatel1@gmail.com>
2024-05-06 16:20:14 -0400

    express server started

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.get('/api', (req, res) => res.json({ message: 'Welcome to the two factor authentication project' }));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
